25adecd2c7f5db60787d5e41d64880de2fd7aa78
taromorimotohf <dev.taro.morimoto@gmail.com>
2025-02-26 20:33:30 +0900

    ✅ chat の test 以外は一旦クリア

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,17 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.tsx?$': ['ts-jest', {
-        tsconfig: 'tsconfig.json',
-        babelConfig: false,
-      }],
-    },
-    testMatch: ['**/?(*.)+(test).ts?(x)'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/app/$1',
-      '\\.(css|scss)$': 'identity-obj-proxy',
-    },
-    transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    ...pathsToModuleNameMapper(compilerOptions.paths || {}, { prefix: '<rootDir>/' }),
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+  ],
+};
